2492f99d84477d7ceeb7cf0b0c48aafd265188de
Julio Herrera <42546972+jurhs2000@users.noreply.github.com>
2023-03-13 09:51:27 -0600

    feat: render wireframe of function

--- main.cpp
+++ main.cpp
@@ -1,100 +1,128 @@
-#include <GL/glut.h>
-#include <iostream>
-#include <omp.h>
+//#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "GL/glut.h"
 
-using namespace std;
-int rx = 100, ry = 125;
-int xCenter = 250, yCenter = 250;
+void init();
+void display();
+void myIdle();
+int main(int argc, char** argv);
+float f(double x, double z);
+void normalVector(float x, float y, float z, float *norm);
 
-void myinit(void)
+float thetaY = 0;
+float thetaX = 20;
+float increment = 0.5;
+float xNear = -6, xFar = 6, yNear = -6, yFar = 6, zNear = -6, zFar = 6;
+float n = 50, m = 50;
+
+void init()
 {
-	glClearColor(1.0, 1.0, 1.0, 0.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0.0, 640.0, 0.0, 480.0);
+    glClearColor(1.0, 1.0, 1.0, 0);
+    glMatrixMode(GL_PROJECTION);
+    glOrtho(xNear, xFar, yNear, yFar, zNear, zFar);
+    glColor3f(0.0,0.0,1.0);
+
 }
 
-void setPixel(GLint x, GLint y)
+float f(double x, double z)
 {
-	glBegin(GL_POINTS);
-	glVertex2i(x, y);
-	glEnd();
+    //return 4*cos(sqrt(x*x+z*z));
+    return (x*x)-(z*z);
 }
-void ellipseMidPoint()
-{
-	float x = 0;
-	float y = ry;
-	float p1 = ry * ry - (rx * rx) * ry + (rx * rx) * (0.25);
-	float dx = 2 * (ry * ry) * x;
-	float dy = 2 * (rx * rx) * y;
-	glColor3ub(rand() % 255, rand() % 255, rand() % 255);
-	while (dx < dy)
-	{
-		setPixel(xCenter + x, yCenter + y);
-		setPixel(xCenter - x, yCenter + y);
-		setPixel(xCenter + x, yCenter - y);
-		setPixel(xCenter - x, yCenter - y);
-		if (p1 < 0)
-		{
-			x = x + 1;
-			dx = 2 * (ry * ry) * x;
-			p1 = p1 + dx + (ry * ry);
-		}
-		else
-		{
-			x = x + 1;
-			y = y - 1;
-			dx = 2 * (ry * ry) * x;
-			dy = 2 * (rx * rx) * y;
-			p1 = p1 + dx - dy + (ry * ry);
-		}
-	}
-	glFlush();
 
-	float p2 = (ry * ry) * (x + 0.5) * (x + 0.5) + (rx * rx) * (y
-		- 1) * (y - 1) - (rx * rx) * (ry * ry);
-	glColor3ub(rand() % 255, rand() % 255, rand() % 255);
-	while (y > 0)
-	{
-		setPixel(xCenter + x, yCenter + y);
-		setPixel(xCenter - x, yCenter + y);
-		setPixel(xCenter + x, yCenter - y);
-		setPixel(xCenter - x, yCenter - y);
-		if (p2 > 0)
-		{
-			x = x;
-			y = y - 1;
-			dy = 2 * (rx * rx) * y;
-			p2 = p2 - dy + (rx * rx);
-		}
-		else
-		{
-			x = x + 1;
-			y = y - 1;
-			dy = dy - 2 * (rx * rx);
-			dx = dx + 2 * (ry * ry);
-			p2 = p2 + dx -
-				dy + (rx * rx);
-		}
-	}
-	glFlush();
+void normalVector(float x, float y, float z, float *norm)
+{
+    norm[0] = 4*sin(sqrt(x*x+z*z))*x/sqrt(x*x+z*z);
+    norm[1] = 1;
+    norm[2] = 4*sin(sqrt(x*x+z*z))*z/sqrt(x*x+z*z);
+    float d = norm[0]*norm[0] + norm[1]*norm[1] + norm[2]*norm[2];
+    if(d > 0)
+        for (int k = 0; k < 3; k++)
+            norm[k]/=sqrt(d);
 }
+
 void display()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0, 0.0, 0.0);
-	glPointSize(2.0);
-	ellipseMidPoint();
-	glFlush();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glRotatef(thetaX,1,0,0);
+    glRotatef(thetaY,0,1,0);
+    float norm[3];
+    double xGap=(xNear-xFar)/n;
+    double zGap=(zNear-zFar)/m;
+    for (int i=0; i<n; i++)
+    {
+        for (int j=0; j<m; j++)
+        {
+            glBegin(GL_LINE_LOOP);
+                float x = xFar + i*xGap;
+                float z = zFar + j*zGap;
+                float y = f(x,z);
+                glVertex3f(x,y,z);
+                normalVector(x,y,z,norm);
+                glNormal3fv(norm);
+                x = xFar + i*xGap;
+                z = zFar + (j+1)*zGap;
+                y = f(x,z);
+                glVertex3f(x,y,z);
+                normalVector(x,y,z,norm);
+                glNormal3fv(norm);
+                x = xFar + (i+1)*xGap;
+                z = zFar + (j+1)*zGap;
+                y = f(x,z);
+                glVertex3f(x,y,z);
+                normalVector(x,y,z,norm);
+                glNormal3fv(norm);
+            glEnd();
+            glBegin(GL_LINE_LOOP);
+                x = xFar + i*xGap;
+                z = zFar + j*zGap;
+                y = f(x,z);
+                glVertex3f(x,y,z);
+                normalVector(x,y,z,norm);
+                glNormal3fv(norm);
+                x = xFar + (i+1)*xGap;
+                z = zFar + (j+1)*zGap;
+                y = f(x,z);
+                glVertex3f(x,y,z);
+                normalVector(x,y,z,norm);
+                glNormal3fv(norm);
+                x = xFar + (i+1)*xGap;
+                z = zFar + (j)*zGap;
+                y = f(x,z);
+                glVertex3f(x,y,z);
+                normalVector(x,y,z,norm);
+                glNormal3fv(norm);
+            glEnd();
+        }
+    }
+    glutSwapBuffers();
 }
+
+void myIdle()
+{
+    double zzz=0;
+    thetaY += increment;
+    if(thetaY > 360.0)
+        thetaY -= 360.0;
+    for (int i=0; i<1000000; i++)
+        zzz = sqrt((double)i);
+    glutPostRedisplay();
+}
+
 int main(int argc, char** argv)
 {
-	glutInit(&argc, argv);
-	glutInitWindowSize(640, 480);
-	glutInitWindowPosition(10, 10);
-	glutCreateWindow("User_Name");
-	myinit();
-	glutDisplayFunc(display);
-	glutMainLoop();
-	return 0;
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
+    glutInitWindowSize(800,800);
+    glutInitWindowPosition(10,10);
+    glutCreateWindow("3D Wireframe Solids");
+    glutDisplayFunc(display);
+    glutIdleFunc(myIdle);
+    init();
+    glutMainLoop();
+    return 0;
 }
